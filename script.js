8b7d46697c62683d4d9dcdd8e2ce02b4f3d578c2
cankilic-gh <cankilic.mail@gmail.com>
2021-06-01 14:47:44 +0300

    It works

--- script.js
+++ script.js
@@ -43,11 +43,19 @@ let app = new Vue({
 
         flippedCards: [],
 
-        finish: false
+        finish: false,
+
+        start: false,
+        turns: 0,
+        totalTime: {
+            minutes: 0,
+            seconds: 0,
+        },
         
     },
 
     created() {
+
         this.cards.forEach((card) => {
             Vue.set(card, 'isFlipped', false)
         });
@@ -61,10 +69,37 @@ let app = new Vue({
     },
 
     methods: {
+
+        reset() {
+            clearInterval(this.interval);
+
+            this.cards.forEach((card) => {
+                Vue.set(card, 'isFlipped', false);
+                Vue.set(card, 'isMatched', false);
+            });
+
+            setTimeout(() => {
+                this.memoryCards = [];
+                this.memoryCards = _.shuffle(this.memoryCards.concat(_.cloneDeep(this.cards), _.cloneDeep(this.cards)));
+                this.totalTime.minutes = 0;
+                this.totalTime.seconds = 0;
+                this.start = false;
+                this.finish = false;
+                this.turns = 0;
+                this.flippedCards = [];
+
+            }, 600);
+
+        },
+
         flipCard(card) {
 
             if (card.isMatched || card.isFlipped || this.flippedCards.length === 2)
                 return;
+
+            if (!this.start) {
+                this._startGame();
+            }
 
             card.isFlipped = true;
 
@@ -82,6 +117,7 @@ let app = new Vue({
 
                     //All cards matched ?
                     if (this.memoryCards.every(card => card.isMatched === true)) {
+                        clearInterval(this.interval);
                         this.finish = true;
                     }
 
@@ -93,9 +129,44 @@ let app = new Vue({
                     this.flippedCards = [];
                 }, 800);
             }
-        }
+
+            this.turns++;
+        },
+
+        _startGame() {
+            this._tick();
+            this.interval = setInterval(this._tick, 1000);
+            this.start = true;
+        },
+
+        _tick() {
+            if (this.totalTime.seconds !== 59) {
+                this.totalTime.seconds++;
+                return
+            }
+
+            this.totalTime.minutes++;
+            this.totalTime.seconds = 0;
+        },
 
     },
+
+    computed: {
+        sec() {
+            if (this.totalTime.seconds < 10) {
+                return '0' + this.totalTime.seconds;
+            }
+            return this.totalTime.seconds;
+        },
+        min() {
+            if (this.totalTime.minutes < 10) {
+                return '0' + this.totalTime.minutes;
+            }
+            return this.totalTime.minutes;
+        }
+    },
+
+    
 
 });
 
